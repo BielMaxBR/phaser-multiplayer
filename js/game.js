67b89154a5010cbc82e7c672d71046cefc20297d
Gabriel Maximo <gbielmaximo@gmail.com>
2021-05-21 23:14:24 +0000

    pausa pra dps

---
+++ js/game.js
@@ -0,0 +1,9 @@
+import Game from '/js/scenes/Game.js'
+
+const game = new Phaser.Game({
+  type: Phaser.WEBGL,
+  width: 16*32,
+  height: 600,
+  canvas: document.getElementById('game'),
+  scene: [Game]
+})
---
+++ js/scenes/Game.js
@@ -0,0 +1,5 @@
+export default class Game extends Phaser.Scene {
+  constructor() {
+    super("game")
+  }
+} 
