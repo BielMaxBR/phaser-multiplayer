d1b16af957f441bf0307e0cebbdc0d5964b3a2a5
Gabriel Maximo <gbielmaximo@gmail.com>
2021-05-21 23:04:45 +0000

    links

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 let express = require('express'),
   app = express(),
   server = require('http').Server(app),
-  io = require('socket.io')(server)
+  io = require('socket.io')(server);
+
+app.use('/css',express.static(__dirname + '/css'));
+app.use('/js',express.static(__dirname + '/js'));
+app.use('/assets',express.static(__dirname + '/assets'));
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/index.html');
+});
 
 server.listen(8081,function(){ // Listens to port 8081
   console.log('Listening on '+server.address().port);
