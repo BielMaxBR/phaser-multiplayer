7af1b75bc1967f1d888a6266cd7a7667e5b16a36
Gabriel Maximo <gbielmaximo@gmail.com>
2023-02-23 14:43:03 +0000

    super fit

--- client/src/Utils/PhaserConfig.ts
+++ client/src/Utils/PhaserConfig.ts
@@ -1,16 +1,20 @@
 export interface GameOptions {
+    scale: object;
     type: number;
     width: number;
     height: number;
     parent: string;
-  }
-  
-  export function getPhaserConfig(): GameOptions {
+}
+
+export function getPhaserConfig(): GameOptions {
     return {
-      type: Phaser.CANVAS,
-      width: window.innerWidth,
-      height: window.innerHeight * 0.9,
-      parent: 'game',
+        scale: {
+            mode: Phaser.Scale.WIDTH_CONTROLS_HEIGHT,
+            autoCenter: Phaser.Scale.CENTER_VERTICALLY,
+        },
+        type: Phaser.CANVAS,
+        width: 1280,
+        height: 720,
+        parent: "game",
     };
-  }
-  
+}
