986b9430551152c8032c6657062ecd3a96076553
BielMaxBR <gbielmaximo@gmail.com>
2023-03-16 11:08:52 -0300

    lib instalada com sucesso 😎

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,27 +5,22 @@ import geckos, {
     ServerChannel,
 } from "@geckos.io/server";
 import Game from "./phaser/game.js";
+import { Server, ServerOptions } from "@geckolyseus/server";
+new Game();
 
-new Game()
-
-const config = {
-    cors: { allowAuthorization: true, origin: "*" },
-    iceServers: [
-        { urls: "stun:stun.l.google.com:19302" },
-        { urls: "stun:stun2.l.google.com:19302" },
-        { urls: "stun:stun4.l.google.com:19302" },
-    ],
+const config: ServerOptions = {
+    colyseusOptions: { greet: false },
+    geckosOptions: {
+        cors: { allowAuthorization: true, origin: "*" },
+        iceServers: [
+            { urls: "stun:stun.l.google.com:19302" },
+            { urls: "stun:stun2.l.google.com:19302" },
+            { urls: "stun:stun4.l.google.com:19302" },
+        ],
+    },
 };
-const io: GeckosServer = geckos(config);
 
-io.onConnection((channel: ServerChannel) => {
-    console.log("ligado");
-    channel.on("message", (data: Data, senderId: ChannelId) => {
-        console.log(data);
-    });
-});
-
-io.listen(9208);
-export default io;
+const server = new Server(config);
+server.listenAll(4000, 4100);
 
 console.log("Avara quedava");
